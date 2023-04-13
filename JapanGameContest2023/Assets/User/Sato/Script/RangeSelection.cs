a2f5bb61a0db0f31e39b3e9642dcba788edf671f
satoAFP <72423443+satoAFP@users.noreply.github.com>
2023-04-13 16:32:49 +0900

    色んな微調整

--- JapanGameContest2023/Assets/User/Sato/Script/RangeSelection.cs
+++ JapanGameContest2023/Assets/User/Sato/Script/RangeSelection.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class RangeSelection : MonoBehaviour
 {
-    [SerializeField, Header("範囲選択表示用オブジェクト")]
-    private GameObject selectionObj;
+    [SerializeField, Header("ドット描画時の間隔")] private float wide;
 
+    
+    //最初の一回しか通らない処理用
     private bool first = true;
-    private Vector3 clickStartPos;
-    private GameObject clone;
-    private bool selectionMode = false;
-    private bool editMode = false;
-    private Vector3 beforePos = new Vector3(0, 0, 0);
+    private bool first2 = true;
+
+    private Vector3 clickStartPos;                      //クリック開始時の初期位置
+    private GameObject clone;                           //選択範囲表示用オブジェクト
+    private bool selectionMode = false;                 //オブジェクトを選択しているかどうか
+    private bool editMode = false;                      //オブジェクトを選択しているかどうか
+    private Vector3 beforePos = new Vector3(0, 0, 0);   //一フレーム前のマウスの位置
+
+    private List<GameObject> cloneDot = new List<GameObject>();//ドット格納用
+    private Vector2 startPos;                           //クリックしたときの初期位置
+    private Vector2 square;                             //四角の縦横の長さ
+    private int dotNum;                                 //ドットの数格納用
 
 
     // Update is called once per frame
@@ -60,9 +68,12 @@ public class RangeSelection : MonoBehaviour
 
                     //選択されているオブジェクトに加算
                     for (int i = 0; i < Objs.Count; i++)
-                    {
                         Objs[i].transform.localPosition += movePower;
-                    }
+
+                    //ドットの枠にも移動量加算
+                    for (int i = 0; i < cloneDot.Count; i++) 
+                        cloneDot[i].transform.localPosition += movePower;
+
                 }
             }
             //オブジェクトが選択されていない時
@@ -70,6 +81,8 @@ public class RangeSelection : MonoBehaviour
             {
                 //オブジェクト選択範囲表示
                 SelectObj();
+                //ドットの枠表示
+                Dotline();
             }
         }
 
@@ -82,15 +95,11 @@ public class RangeSelection : MonoBehaviour
     private void SelectObj()
     {
         GameObject selectUI = managerAccessor.Instance.dataMagager.rightClickUIClone;
-        if (selectUI != null &&
+        if (!(selectUI != null &&
             selectUI.GetComponent<RectTransform>().position.x + (selectUI.GetComponent<RectTransform>().sizeDelta.x / 2) > Input.mousePosition.x &&
             selectUI.GetComponent<RectTransform>().position.x - (selectUI.GetComponent<RectTransform>().sizeDelta.x / 2) < Input.mousePosition.x &&
             selectUI.GetComponent<RectTransform>().position.y + (selectUI.GetComponent<RectTransform>().sizeDelta.y / 2) > Input.mousePosition.y &&
-            selectUI.GetComponent<RectTransform>().position.y - (selectUI.GetComponent<RectTransform>().sizeDelta.y / 2) < Input.mousePosition.y)
-        {
-
-        }
-        else
+            selectUI.GetComponent<RectTransform>().position.y - (selectUI.GetComponent<RectTransform>().sizeDelta.y / 2) < Input.mousePosition.y))
         {
             //長押しで選択範囲表示
             if (Input.GetMouseButton(0))
@@ -104,7 +113,7 @@ public class RangeSelection : MonoBehaviour
                     clickStartPos = Input.mousePosition;
 
                     //範囲選択用オブジェクトの初期座標設定
-                    clone = Instantiate(selectionObj);
+                    clone = Instantiate(managerAccessor.Instance.objDataManager.selectionObj);
                     clone.transform.localPosition = managerAccessor.Instance.dataMagager.MouseWorldChange();
 
                     //長押し状態を解除するまではいらないようにする
@@ -140,5 +149,137 @@ public class RangeSelection : MonoBehaviour
     }
 
 
+    //ドットの枠表示用関数
+    private void Dotline()
+    {
+        if (!managerAccessor.Instance.dataMagager.playMode)
+        {
+            //選択開始時の1フレーム目のマウスの座標取得
+            if (Input.GetMouseButton(0))
+            {
+                if (first2)
+                {
+                    startPos = managerAccessor.Instance.dataMagager.MouseWorldChange();
+                    first2 = false;
+                }
+            }
+            else
+                first2 = true;
+
+            //範囲選択処理
+            if (Input.GetMouseButton(0))
+            {
+                //DataManager取得
+                DataManager dataManager = managerAccessor.Instance.dataMagager;
+                //初期位置代入
+                Vector2 usePos = startPos;
+                //クリック後、startPosを原点に縦横それぞれの位置チェック用
+                int checkPos = 0;
+
+                //四角のサイズ計算
+                square.x = Mathf.Abs(dataManager.MouseWorldChange().x - startPos.x);
+                square.y = Mathf.Abs(dataManager.MouseWorldChange().y - startPos.y);
+
+                //ドットを打つ数を計算
+                dotNum = (int)((square.x * 2 + square.y * 2) / wide);
+                //初期位置分ずらす
+                square += startPos;
+
+                //ドットの初期化
+                for (int i = 0; i < cloneDot.Count; i++)
+                    Destroy(cloneDot[i]);
+                cloneDot.Clear();
+
+                //startPosから見て左下にいた時
+                if (dataManager.MouseWorldChange().x < startPos.x && dataManager.MouseWorldChange().y < startPos.y)
+                    checkPos = 1;
+                //startPosから見て左上にいた時
+                else if (dataManager.MouseWorldChange().x < startPos.x)
+                    checkPos = 2;
+                //startPosから見て右下にいた時
+                else if (dataManager.MouseWorldChange().y < startPos.y)
+                    checkPos = 3;
+
+                //ドットを描画
+                for (int i = 0; i < dotNum; i++)
+                {
+                    //下
+                    if (usePos.x < square.x && usePos.y == startPos.y)
+                    {
+                        //ドットを打つ座標をずらす
+                        usePos.x += wide;
+
+                        //もし四角からはみ出した場合、出た分を次の描画位置に修正する
+                        if (usePos.x > square.x)
+                        {
+                            usePos.y += usePos.x - square.x;
+                            usePos.x = square.x;
+                        }
+                    }
+                    //右
+                    else if (usePos.y < square.y && usePos.x == square.x)
+                    {
+                        usePos.y += wide;
+
+                        if (usePos.y > square.y)
+                        {
+                            usePos.x -= usePos.y - square.y;
+                            usePos.y = square.y;
+                        }
+                    }
+                    //上
+                    else if (usePos.x > startPos.x && usePos.y == square.y)
+                    {
+                        usePos.x -= wide;
+
+                        if (usePos.x < startPos.x)
+                        {
+                            usePos.y -= startPos.x - usePos.x;
+                            usePos.x = startPos.x;
+                        }
+                    }
+                    //左
+                    else if (usePos.y > startPos.y && usePos.x == startPos.x)
+                    {
+                        usePos.y -= wide;
+
+                        if (usePos.y < startPos.y)
+                        {
+                            usePos.x -= startPos.y - usePos.y;
+                            usePos.y = startPos.y;
+                        }
+                    }
+
+                    //修正値代入用
+                    Vector2 inUsePos = usePos;
+
+                    //startPosから見て左下にいた時
+                    if (checkPos == 1)
+                    {
+                        inUsePos.x = -usePos.x;
+                        inUsePos.y = -usePos.y;
+                        inUsePos += startPos * 2;
+                    }
+                    //startPosから見て左上にいた時
+                    else if (checkPos == 2)
+                    {
+                        inUsePos.x = -usePos.x;
+                        inUsePos.x += startPos.x * 2;
+                    }
+                    //startPosから見て右下にいた時
+                    else if (checkPos == 3)
+                    {
+                        inUsePos.y = -usePos.y;
+                        inUsePos.y += startPos.y * 2;
+                    }
+
+                    //ドットの描画
+                    cloneDot.Add(Instantiate(managerAccessor.Instance.objDataManager.dotObj));
+                    cloneDot[i].transform.position = inUsePos;
+                }
+
+            }
+        }
+    }
 
 }
