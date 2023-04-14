2b85fe4bf9d8a30b05e71afb6122e268c90bf718
yamakan_z <h1202016006@hiratagakuen.onmicrosoft.com>
2023-04-14 16:33:45 +0900

    rayの判定とりあえず

--- JapanGameContest2023/Assets/User/Yamamoto/Scripts/AI.cs
+++ JapanGameContest2023/Assets/User/Yamamoto/Scripts/AI.cs
@@ -13,33 +13,42 @@ public class AI : MonoBehaviour
     // 移動中かどうかのフラグ
     private bool isMoving = false;
 
-    public Vector2 origin;//rayの原点
-
-    public Vector2 origin2;//rayの原点
+    [SerializeField] private Vector2 origin;//rayの原点
 
     private Vector2 direction;//rayの方向ベクトル
+
+    [SerializeField] private LayerMask[] layermask;//レイヤーマスク
+
+    int layerMask = ~(1 << 8);
 
     [SerializeField,Header("テスト用Rayの長さ調整")]
     private float ray_length = 5.0f;
 
     void Update()
     {
-        origin = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y-1.0f);
+        origin = transform.position;
 
-        direction = new Vector2(1, 0);//X方向を指す
+        direction = transform.right;//X方向を指す
+
+        RaycastHit2D hit = Physics2D.Raycast(origin, direction, ray_length, layermask[0]);
+
+        // Rayの可視化
+        Debug.DrawLine(origin, origin + direction * ray_length, Color.red);
+
+        if (hit.collider != null)
+        {
+            Debug.DrawLine(origin, hit.point, Color.green);
+
+            // 当たったオブジェクトが自身でなければ、何かしらの処理をする
+            if (hit.collider.gameObject != gameObject)
+            {
+                Debug.Log("Hit object: " + hit.collider.gameObject.name);
+            }
+
+        }
 
 
-        Ray2D ray = new Ray2D(origin, direction);//rayを生成
-
-        //RaycastHit2D hit = Physics2D.Raycast(firstpos, Vector2.right);
-
-        Debug.DrawRay(ray.origin, ray.direction * ray_length, Color.yellow);
-
-
-        //if (hit.collider != null)
-        //{
-        //    Debug.Log("atari");
-        //}
+        // Debug.DrawRay(ray.origin, ray.direction * ray_length, Color.yellow);
 
         // 移動中でなければクリックを受け付ける
         if (!isMoving && Input.GetMouseButtonDown(0))
