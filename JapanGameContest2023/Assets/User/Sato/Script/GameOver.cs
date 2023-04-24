c687c58f507820440765f084e590ae060f6bcf09
AFP <h1202019007@hiratagakuen.onmicrosoft.com>
2023-04-24 14:15:42 +0900

    ゲームオーバー画面

--- JapanGameContest2023/Assets/User/Sato/Script/GameOver.cs
+++ JapanGameContest2023/Assets/User/Sato/Script/GameOver.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class GameOver : MonoBehaviour
 {
-    [SerializeField, Header("GameOverPanel")] private GameObject gameoverPanel;
-
     // Update is called once per frame
     void Update()
     {
         if (managerAccessor.Instance.dataMagager.playerlost == true) 
         {
-            gameoverPanel.SetActive(true);
+            gameObject.SetActive(true);
         }
 
 
