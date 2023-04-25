76ecaeab89fe0d4160c22fb6e0fad815cb4718d1
satoAFP <72423443+satoAFP@users.noreply.github.com>
2023-04-25 17:54:26 +0900

    心折れそう

--- JapanGameContest2023/Assets/User/Sato/Script/RangeSelection.cs
+++ JapanGameContest2023/Assets/User/Sato/Script/RangeSelection.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RangeSelection : MonoBehaviour
 {
+    //サイズ変更時持っている縁の位置の名前
     public enum ChangeSizePosName
     {
         DOWN,
@@ -17,6 +18,15 @@ public class RangeSelection : MonoBehaviour
         NONE,
     }
 
+    public enum MouseDirection
+    {
+        RIGHT_UP,
+        LEFT_DOWN,
+        LEFT_UP,
+        RIGHT_DOWN,
+        NONE,
+    }
+
 
     [SerializeField, Header("ドット描画時の間隔")] private float wide;
 
@@ -27,6 +37,7 @@ public class RangeSelection : MonoBehaviour
     private bool first2 = true;
     private bool first3 = false;
     private bool first4 = true;
+    private bool first5 = true;
 
     private List<GameObject> Objs=new List<GameObject>();//選択されているオブジェクト格納用
     private Vector3 clickStartPos;                      //クリック開始時の初期位置
@@ -34,8 +45,8 @@ public class RangeSelection : MonoBehaviour
     private bool selectionMode = false;                 //オブジェクトを選択しているかどうか
     private bool editMode = false;                      //オブジェクトを選択しているかどうか
     private Vector3 beforePos = new Vector3(0, 0, 0);   //一フレーム前のマウスの位置
-    private Vector2 judgeStartPos;                      //掴める範囲(左下)
-    private Vector2 judgeEndPos;                        //掴める範囲(右上)
+    [SerializeField] private Vector2 judgeStartPos;                      //掴める範囲(左下)
+    [SerializeField] private Vector2 judgeEndPos;                        //掴める範囲(右上)
     private bool onEdge = false;                        //ドットの枠の縁に乗っているとき
     private int onPos = (int)ChangeSizePosName.NONE;    //今乗っている縁の場所(列挙参照)
     private Vector2 onStartPos;                         //拡大縮小時の初期位置
@@ -44,13 +55,15 @@ public class RangeSelection : MonoBehaviour
     private List<Vector3> mempos = new List<Vector3>(); //選択されているオブジェクトの位置データ
     private Vector2 memjudgeStartPos;                   //選択されているオブジェクトの掴める範囲(左下)
     private Vector2 memjudgeEndPos;                     //選択されているオブジェクトの掴める範囲(右上)
+    private bool changeSizeEnd = false;                 //サイズ変更終了時切り替わる
+
 
     private List<GameObject> cloneDot = new List<GameObject>();//ドット格納用
     private Vector2 startPos;                           //クリックしたときの初期位置
     private Vector2 usePos;                             //初期位置代入
     private Vector2 square;                             //四角の縦横の長さ
     private int checkPos = 0;                           //クリック後、startPosを原点に縦横それぞれの位置チェック用
-    private int dotNum;                                 //ドットの数格納用
+    [SerializeField] private int dotNum;                                 //ドットの数格納用
     private Vector2 inUsePos;                           //usePosの修正値代入用
 
 
@@ -99,10 +112,11 @@ public class RangeSelection : MonoBehaviour
             //オブジェクトが選択されている時
             if (editMode)
             {
+
                 if (onEdge)
                 {
                     ChangeRangeSize();
-
+                    
                 }
                 else
                 {
@@ -131,6 +145,7 @@ public class RangeSelection : MonoBehaviour
             //オブジェクトが選択されていない時
             else
             {
+                Debug.Log("koko");
                 //オブジェクト選択範囲表示
                 SelectObj();
                 //ドットの枠表示
@@ -226,7 +241,7 @@ public class RangeSelection : MonoBehaviour
                 //初期位置代入
                 usePos = startPos;
                 //クリック後、startPosを原点に縦横それぞれの位置チェック用
-                checkPos = 0;
+                checkPos = (int)MouseDirection.RIGHT_UP;
 
                 //四角のサイズ計算
                 square.x = Mathf.Abs(dataManager.MouseWorldChange().x - startPos.x);
@@ -247,17 +262,17 @@ public class RangeSelection : MonoBehaviour
                 //startPosから見て左下にいた時
                 if (dataManager.MouseWorldChange().x < startPos.x && dataManager.MouseWorldChange().y < startPos.y)
                 {
-                    checkPos = 1;
+                    checkPos = (int)MouseDirection.LEFT_DOWN;
                 }
                 //startPosから見て左上にいた時
                 else if (dataManager.MouseWorldChange().x < startPos.x)
                 {
-                    checkPos = 2;
+                    checkPos = (int)MouseDirection.LEFT_UP;
                 }
                 //startPosから見て右下にいた時
                 else if (dataManager.MouseWorldChange().y < startPos.y)
                 {
-                    checkPos = 3;
+                    checkPos = (int)MouseDirection.RIGHT_DOWN;
                 }
 
                 //ドットの描画
@@ -282,19 +297,19 @@ public class RangeSelection : MonoBehaviour
                 if(first3)
                 {
                     //右上
-                    if (checkPos == 0)
+                    if (checkPos == (int)MouseDirection.RIGHT_UP)
                     {
                         judgeStartPos = startPos;
                         judgeEndPos = managerAccessor.Instance.dataMagager.MouseWorldChange();
                     }
                     //左下
-                    else if (checkPos == 1)
+                    else if (checkPos == (int)MouseDirection.LEFT_DOWN)
                     {
                         judgeStartPos = managerAccessor.Instance.dataMagager.MouseWorldChange();
                         judgeEndPos = startPos;
                     }
                     //左上
-                    else if (checkPos == 2)
+                    else if (checkPos == (int)MouseDirection.LEFT_UP)
                     {
                         judgeStartPos.x = managerAccessor.Instance.dataMagager.MouseWorldChange().x;
                         judgeStartPos.y = startPos.y;
@@ -302,7 +317,7 @@ public class RangeSelection : MonoBehaviour
                         judgeEndPos.y = managerAccessor.Instance.dataMagager.MouseWorldChange().y;
                     }
                     //右下
-                    else if (checkPos == 3)
+                    else if (checkPos == (int)MouseDirection.RIGHT_DOWN)
                     {
                         judgeStartPos.x = startPos.x;
                         judgeStartPos.y = managerAccessor.Instance.dataMagager.MouseWorldChange().y;
@@ -353,82 +368,113 @@ public class RangeSelection : MonoBehaviour
             //初期位置から離れた距離
             Vector2 setStartPos = judgeStartPos;
 
+            //描画の初期位置を左下に設定
+            checkPos = (int)MouseDirection.RIGHT_UP;
+
+
             //四角のサイズ変更時それぞれの数値変更
             if (onPos == (int)ChangeSizePosName.DOWN) 
             {
                 //四角のサイズ変更
                 square.x = backUpSquare.x;
-                square.y = backUpSquare.y - (dataManager.MouseWorldChange().y - onStartPos.y);
+                square.y = Mathf.Abs(backUpSquare.y - (dataManager.MouseWorldChange().y - onStartPos.y));
                 //初期位置の設定
                 setStartPos.y = startPos.y + dataManager.MouseWorldChange().y - onStartPos.y;
                 //ゲーム全体で判定をする四角の座標更新
                 judgeStartPos.y = setStartPos.y;
                 //サイズ変更時の移動量設定
                 BlockRatioChange(new Vector3(0, -(dataManager.MouseWorldChange().y - onStartPos.y), 0), onPos);
+                if (judgeEndPos.y < dataManager.MouseWorldChange().y)
+                {
+                    checkPos = (int)MouseDirection.RIGHT_DOWN;
+                }
             }
             if (onPos == (int)ChangeSizePosName.RIGHT)
             {
-                square.x = backUpSquare.x + (dataManager.MouseWorldChange().x - onStartPos.x);
+                square.x = Mathf.Abs(backUpSquare.x + (dataManager.MouseWorldChange().x - onStartPos.x));
                 square.y = backUpSquare.y;
                 judgeEndPos.x = setStartPos.x + square.x;
                 //サイズ変更時の移動量設定
                 BlockRatioChange(new Vector3(dataManager.MouseWorldChange().x - onStartPos.x, 0, 0), onPos);
+
+                if (judgeStartPos.x > dataManager.MouseWorldChange().x)
+                {
+                    checkPos = (int)MouseDirection.LEFT_UP;
+                }
             }
             if (onPos == (int)ChangeSizePosName.UP)
             {
                 square.x = backUpSquare.x;
-                square.y = backUpSquare.y + (dataManager.MouseWorldChange().y - onStartPos.y);
+                square.y = Mathf.Abs(backUpSquare.y + (dataManager.MouseWorldChange().y - onStartPos.y));
                 judgeEndPos.y = setStartPos.y + square.y;
                 //サイズ変更時の移動量設定
                 BlockRatioChange(new Vector3(0, dataManager.MouseWorldChange().y - onStartPos.y, 0), onPos);
+
+                if (judgeStartPos.y > dataManager.MouseWorldChange().y)
+                {
+                    checkPos = (int)MouseDirection.RIGHT_DOWN;
+                }
             }
             if (onPos == (int)ChangeSizePosName.LEFT)
             {
-                square.x = backUpSquare.x - (dataManager.MouseWorldChange().x - onStartPos.x);
+                square.x = Mathf.Abs(backUpSquare.x - (dataManager.MouseWorldChange().x - onStartPos.x));
                 square.y = backUpSquare.y;
                 setStartPos.x = startPos.x + dataManager.MouseWorldChange().x - onStartPos.x;
                 judgeStartPos.x = setStartPos.x;
                 //サイズ変更時の移動量設定
                 BlockRatioChange(new Vector3(-(dataManager.MouseWorldChange().x - onStartPos.x), 0, 0), onPos);
+
+                if (judgeEndPos.x < dataManager.MouseWorldChange().x)
+                {
+                    checkPos = (int)MouseDirection.LEFT_UP;
+                }
             }
             if (onPos == (int)ChangeSizePosName.RIGHT_DOWN)
             {
-                square.x = backUpSquare.x + (dataManager.MouseWorldChange().x - onStartPos.x);
-                square.y = backUpSquare.y - (dataManager.MouseWorldChange().y - onStartPos.y);
+                square.x = Mathf.Abs(backUpSquare.x + (dataManager.MouseWorldChange().x - onStartPos.x));
+                square.y = Mathf.Abs(backUpSquare.y - (dataManager.MouseWorldChange().y - onStartPos.y));
                 setStartPos.y = startPos.y + dataManager.MouseWorldChange().y - onStartPos.y;
                 judgeEndPos.x = setStartPos.x + square.x;
                 judgeStartPos.y = setStartPos.y;
                 //サイズ変更時の移動量設定
                 BlockRatioChange(new Vector3(dataManager.MouseWorldChange().x - onStartPos.x, -(dataManager.MouseWorldChange().y - onStartPos.y), 0), onPos);
+
+                checkPos = GetCheckPos(new Vector2(judgeStartPos.x, judgeEndPos.y), dataManager.MouseWorldChange());
             }
             if (onPos == (int)ChangeSizePosName.RIGHT_UP)
             {
-                square.x = backUpSquare.x + (dataManager.MouseWorldChange().x - onStartPos.x);
-                square.y = backUpSquare.y + (dataManager.MouseWorldChange().y - onStartPos.y);
+                square.x = Mathf.Abs(backUpSquare.x + (dataManager.MouseWorldChange().x - onStartPos.x));
+                square.y = Mathf.Abs(backUpSquare.y + (dataManager.MouseWorldChange().y - onStartPos.y));
                 judgeEndPos.x = setStartPos.x + square.x;
                 judgeEndPos.y = setStartPos.y + square.y;
                 //サイズ変更時の移動量設定
                 BlockRatioChange(new Vector3(dataManager.MouseWorldChange().x - onStartPos.x, dataManager.MouseWorldChange().y - onStartPos.y, 0), onPos);
+
+                checkPos = GetCheckPos(new Vector2(judgeStartPos.x, -judgeStartPos.y), new Vector2(dataManager.MouseWorldChange().x, -dataManager.MouseWorldChange().y));
             }
             if (onPos == (int)ChangeSizePosName.LEFT_UP)
             {
-                square.x = backUpSquare.x - (dataManager.MouseWorldChange().x - onStartPos.x);
-                square.y = backUpSquare.y + (dataManager.MouseWorldChange().y - onStartPos.y);
+                square.x = Mathf.Abs(backUpSquare.x - (dataManager.MouseWorldChange().x - onStartPos.x));
+                square.y = Mathf.Abs(backUpSquare.y + (dataManager.MouseWorldChange().y - onStartPos.y));
                 setStartPos.x = startPos.x + dataManager.MouseWorldChange().x - onStartPos.x;
                 judgeStartPos.x = setStartPos.x;
                 judgeEndPos.y = setStartPos.y + square.y;
                 //サイズ変更時の移動量設定
                 BlockRatioChange(new Vector3(-(dataManager.MouseWorldChange().x - onStartPos.x), dataManager.MouseWorldChange().y - onStartPos.y, 0), onPos);
+
+                checkPos = GetCheckPos(new Vector2(-judgeEndPos.x, -judgeStartPos.y), -dataManager.MouseWorldChange());
             }
             if (onPos == (int)ChangeSizePosName.LEFT_DOWN)
             {
-                square.x = backUpSquare.x - (dataManager.MouseWorldChange().x - onStartPos.x);
-                square.y = backUpSquare.y - (dataManager.MouseWorldChange().y - onStartPos.y);
+                square.x = Mathf.Abs(backUpSquare.x - (dataManager.MouseWorldChange().x - onStartPos.x));
+                square.y = Mathf.Abs(backUpSquare.y - (dataManager.MouseWorldChange().y - onStartPos.y));
                 setStartPos.x = startPos.x + dataManager.MouseWorldChange().x - onStartPos.x;
                 setStartPos.y = startPos.y + dataManager.MouseWorldChange().y - onStartPos.y;
                 judgeStartPos = setStartPos;
                 //サイズ変更時の移動量設定
                 BlockRatioChange(new Vector3(-(dataManager.MouseWorldChange().x - onStartPos.x), -(dataManager.MouseWorldChange().y - onStartPos.y), 0), onPos);
+
+                checkPos = GetCheckPos(new Vector2(-judgeEndPos.x, judgeEndPos.y), new Vector2(-dataManager.MouseWorldChange().x, dataManager.MouseWorldChange().y));
             }
 
             //サイズ変更適用処理
@@ -439,7 +485,7 @@ public class RangeSelection : MonoBehaviour
 
 
             //ドットを打つ数を計算
-            dotNum = (int)((square.x * 2 + square.y * 2) / wide);
+            dotNum = (int)((Mathf.Abs(square.x * 2) + Mathf.Abs(square.y * 2)) / wide);
             //初期位置分ずらす
             square += setStartPos;
 
@@ -450,12 +496,50 @@ public class RangeSelection : MonoBehaviour
                 Destroy(cloneDot[i]);
             cloneDot.Clear();
 
-            //描画の初期位置を左下に設定
-            checkPos = 0;
+            
 
             //ドットの描画
             DotDraw(setStartPos, usePos, square, dotNum);
 
+            first5 = true;
+        }
+        else
+        {
+            if(first5)
+            {
+                Vector2 _judgeStartPos = judgeStartPos;
+                Vector2 _judgeEndPos = judgeEndPos;
+
+                Debug.Log(checkPos);
+
+                //右上
+                if (checkPos == (int)MouseDirection.RIGHT_UP)
+                {
+                }
+                //左下
+                else if (checkPos == (int)MouseDirection.LEFT_DOWN)
+                {
+                    judgeStartPos = _judgeEndPos;
+                    judgeEndPos = _judgeStartPos;
+                    Debug.Log("aaa");
+                }
+                //左上
+                else if (checkPos == (int)MouseDirection.LEFT_UP)
+                {
+                    judgeStartPos.x = _judgeEndPos.x;
+                    judgeEndPos.x = _judgeStartPos.x;
+                    Debug.Log("bbb");
+                }
+                //右下
+                else if (checkPos == (int)MouseDirection.RIGHT_DOWN)
+                {
+                    judgeStartPos.y = _judgeEndPos.y;
+                    judgeEndPos.y = _judgeStartPos.y;
+                    Debug.Log("ccc");
+                }
+
+                first5 = false;
+            }
         }
     }
 
@@ -517,20 +601,20 @@ public class RangeSelection : MonoBehaviour
             inUsePos = UsePos;
 
             //startPosから見て左下にいた時
-            if (checkPos == 1)
+            if (checkPos == (int)MouseDirection.LEFT_DOWN)
             {
                 inUsePos.x = -UsePos.x;
                 inUsePos.y = -UsePos.y;
                 inUsePos += StartPos * 2;
             }
             //startPosから見て左上にいた時
-            else if (checkPos == 2)
+            else if (checkPos == (int)MouseDirection.LEFT_UP)
             {
                 inUsePos.x = -UsePos.x;
                 inUsePos.x += StartPos.x * 2;
             }
             //startPosから見て右下にいた時
-            else if (checkPos == 3)
+            else if (checkPos == (int)MouseDirection.RIGHT_DOWN)
             {
                 inUsePos.y = -UsePos.y;
                 inUsePos.y += StartPos.y * 2;
@@ -720,6 +804,39 @@ public class RangeSelection : MonoBehaviour
                 }
             }
         }
+    }
+
+
+    //原点から見てマウスが何処にいるかチェックする関数
+    //引数1　原点
+    //引数2　マウスの座標
+    //戻り値　現在のマウスの方向
+    private int GetCheckPos(Vector2 origin,Vector2 mouse)
+    {
+        int checkpos = 0;
+
+        //原点から見て右上にいるとき
+        if (origin.x < mouse.x && origin.y < mouse.y) 
+        {
+            checkpos = (int)MouseDirection.RIGHT_DOWN;
+        }
+        //原点から見て左下にいるとき
+        else if (origin.x > mouse.x && origin.y > mouse.y)
+        {
+            checkpos = (int)MouseDirection.LEFT_UP;
+        }
+        //原点から見て左上にいるとき
+        else if (origin.x > mouse.x && origin.y < mouse.y)
+        {
+            checkpos = (int)MouseDirection.LEFT_DOWN;
+        }
+        //原点から見て右下にいるとき
+        else if (origin.x < mouse.x && origin.y > mouse.y)
+        {
+            checkpos = (int)MouseDirection.RIGHT_UP;
+        }
+
+        return checkpos;
     }
 
 
