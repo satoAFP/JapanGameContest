bfaa1680240fee3967e35a94aa5adc69db4c956f
AFP <h1202019007@hiratagakuen.onmicrosoft.com>
2023-04-26 16:22:07 +0900

    カーソル変える途中

--- JapanGameContest2023/Assets/User/Sato/Script/MouseIcon.cs
+++ JapanGameContest2023/Assets/User/Sato/Script/MouseIcon.cs
@@ -4,20 +4,15 @@ using UnityEngine;
 
 public class MouseIcon : MonoBehaviour
 {
-    [SerializeField] private Sprite[] Icon;
+    [SerializeField] private Sprite[] cursor;
+
+    [SerializeField] private Sprite[] arrow;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //gameObject.GetComponent<SpriteRenderer>().sprite = Icon[0];
-
         transform.position = managerAccessor.Instance.dataMagager.MouseWorldChange();
     }
 }
