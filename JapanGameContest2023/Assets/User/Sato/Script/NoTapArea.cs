216f91e2cbf0957c350351b1f9d61486275dc2ca
AFP <h1202019007@hiratagakuen.onmicrosoft.com>
2023-05-10 16:05:45 +0900

    noTapAreaの内容変更

--- JapanGameContest2023/Assets/User/Sato/Script/NoTapArea.cs
+++ JapanGameContest2023/Assets/User/Sato/Script/NoTapArea.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 public class NoTapArea : MonoBehaviour
 {
+    [SerializeField, Header("noTapArea")] private List<GameObject> noTapAreas;
+
     // Update is called once per frame
     void Update()
     {
-        //必要な情報の取得
-        Vector2 pos = gameObject.GetComponent<RectTransform>().position;
-        Vector2 size = gameObject.GetComponent<RectTransform>().sizeDelta;
-        Vector2 mouse = Input.mousePosition;
-
-        //オブジェクト内にカーソルが入っている時、切り替える
-        if (pos.x - (size.x/2) < mouse.x && pos.x + (size.x / 2) > mouse.x &&
-            pos.y - (size.y/2) < mouse.y && pos.y + (size.y / 2) > mouse.y) 
+        for (int i = 0; i < noTapAreas.Count; i++)
         {
-            managerAccessor.Instance.dataMagager.noTapArea = true;
-        }
-        else
-        {
-            managerAccessor.Instance.dataMagager.noTapArea = false;
-        }
+            //必要な情報の取得
+            Vector2 pos = noTapAreas[i].GetComponent<RectTransform>().position;
+            Vector2 size = noTapAreas[i].GetComponent<RectTransform>().sizeDelta;
+            Vector2 mouse = Input.mousePosition;
 
+            //オブジェクト内にカーソルが入っている時、切り替える
+            if (pos.x - (size.x / 2) < mouse.x && pos.x + (size.x / 2) > mouse.x &&
+                pos.y - (size.y / 2) < mouse.y && pos.y + (size.y / 2) > mouse.y)
+            {
+                managerAccessor.Instance.dataMagager.noTapArea = true;
+                break;
+            }
+            else
+            {
+                managerAccessor.Instance.dataMagager.noTapArea = false;
+            }
+        }
 
     }
 }
