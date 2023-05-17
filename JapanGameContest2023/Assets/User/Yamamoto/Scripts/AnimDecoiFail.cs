6d9fe5fed74da8ee88fd53ffdecdb24e2e90dab9
yamakan_z <h1202016006@hiratagakuen.onmicrosoft.com>
2023-05-17 12:06:42 +0900

    Stage1アニメーション完成でございます

--- JapanGameContest2023/Assets/User/Yamamoto/Scripts/AnimDecoiFail.cs
+++ JapanGameContest2023/Assets/User/Yamamoto/Scripts/AnimDecoiFail.cs
@@ -6,15 +6,27 @@ public class AnimDecoiFail : MonoBehaviour
 {
     [SerializeField] private GameObject animdecoifail;//ステージ1用登場アニメーションおとりファイル
 
+    [SerializeField] private Player player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animdecoifail.SetActive(false);//最初は非表示に
+        Invoke(nameof(FileAppear), 0.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if( player.StartAction )
+        {
+            animdecoifail.SetActive(false);//登場アニメーションが終わるとファイル非表示
+        }
     }
+
+    void FileAppear()
+    {
+        animdecoifail.SetActive(true);//ここで表示
+    }
+
 }
